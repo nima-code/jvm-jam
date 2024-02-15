2deb2a0da0dbfefa7d7fd01dd28ca3f8801f1fe3
Nike <nikechukwu@gmail.com>
2024-02-14 22:35:51 -0500

    Added Scala by adding ScalaInterface.scala

---
+++ src/main/scala/com/nuge/ScalaInterface.scala
@@ -0,0 +1,13 @@
+package com.nuge
+
+class ScalaExport {
+  def scalaFunction(): String = {
+    "Hello from Scala!"
+  }
+}
+
+object ScalaExportObject {
+  def scalaFunction(): String = {
+    "Hello from Scala!"
+  }
+}
